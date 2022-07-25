67e654f44da880131706141a5e69163eb7665725
subhanrizwan0 <subhanahmed444sherazi@gmail.com>
2022-07-25 16:18:12 +0500

    js assignment

---
+++ 31-34/index.js
@@ -0,0 +1,49 @@
+// question 1
+
+// var date = new Date();
+// console.log(date);
+
+// question 2
+//  var d = prompt("enter the current month").getMonth()
+//  document.write("current month :" + d );
+//  console.log(d);
+
+// question 2
+var d = getMonth();
+document.write("Current Month :" + d )
+console.log(d);
+
+
+// question 3
+
+
+
+
+// question 4
+// var arr = ["sunday","monday","tuesday","wednesday","thursday","friday","saturday"]
+// var arr1 = ["sunday","saturday"]
+// var user = prompt("enter")
+// for (var i = 0; i < arr.length; i++) {
+//    if(user===arr1[i]) {
+//        alert("it,s a funday")
+//    }
+// }console.log(user)
+
+//question 8
+// var d =new Date("dec,31,2022")
+// console.log(d)
+
+
+//question 12 
+// var todey1 = new Date()
+// var b = todey1.setFullYear(1922)
+// document.write("100 years back, it was "+ b)
+
+// question 13
+// var d = new Date("feb,8,2004").getTime() - new Date().getTime()
+// var res = Math.round(d / (1000*60*260*4*365))
+// document.write("your age is" + res)
+// document.write("your birth year is" + )
+
+
+ 
